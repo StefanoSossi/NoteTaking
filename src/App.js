0f9e960f02b2a3c5392f11be7327c2960a91079b
StefanoSossi <tenosossi2012@gmail.com>
2023-06-12 14:13:02 -0400

    [Task-1] Add grid to display notes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,33 @@ import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { addNote } from "./reducers/cartSlice";
 import NoteCard from "./components/noteCard/noteCard";
-
+import Masonry from "react-masonry-css";
+import Grid from "@mui/material/Grid";
 function App() {
 	const notes = useSelector((state) => state.cart.notes);
 	const dispatch = useDispatch();
+	const getColumnSpan = (noteIndex) => {
+		if (noteIndex % 6 === 0) {
+			return 2; // Set column span to 2 for every 6th note
+		}
+		return 1;
+	};
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h2>
-					Cart items:
+				<h2>Cart items:</h2>
+				<Masonry
+					breakpointCols={3}
+					className="my-masonry-grid"
+					columnClassName="my-masonry-grid_column"
+				>
 					{notes.map((note, index) => (
-						<NoteCard key={index} note={note} index={index}></NoteCard>
+						<Grid item key={index} xs={12} sm={6} md={getColumnSpan(index)}>
+							<NoteCard key={index} note={note} index={index}></NoteCard>
+						</Grid>
 					))}
-				</h2>
+				</Masonry>
 				<div>
 					<button
 						onClick={() =>
--- src/reducers/cartSlice.js
+++ src/reducers/cartSlice.js
@@ -7,13 +7,59 @@ export const cartSlice = createSlice({
 			{
 				id: 0,
 				title: "sda",
-				note: "aaa",
+				note: "aaa asdasd asdasd asdasd asdads ",
 				date: "",
 				status: "",
 			},
 			{
 				id: 1,
-				title: "asdsda",
+				title:
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+				note: "ddd",
+				date: "",
+				status: "",
+			},
+			{
+				id: 2,
+				title:
+					"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit",
+				note: "ddd",
+				date: "",
+				status: "",
+			},
+			{
+				id: 3,
+				title:
+					"est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore ",
+				note: "ddd",
+				date: "",
+				status: "",
+			},
+			{
+				id: 4,
+				title:
+					"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those ",
+				note: "ddd",
+				date: "",
+				status: "",
+			},
+			{
+				id: 5,
+				title: "asdetetertsda",
+				note: "ddd",
+				date: "",
+				status: "",
+			},
+			{
+				id: 6,
+				title: "asdsutyutyutyuda",
+				note: "ddd",
+				date: "",
+				status: "",
+			},
+			{
+				id: 7,
+				title: "asdsxcvxcvvcda",
 				note: "ddd",
 				date: "",
 				status: "",
